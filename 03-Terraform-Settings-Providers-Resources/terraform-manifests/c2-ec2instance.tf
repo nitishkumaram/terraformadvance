2e9c772629f200da1823446416c122179181fc31
Nitish Kumar <nitishkumar@outlook.com>
2024-06-08 19:08:01 +0530

    updated ami

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,6 +1,6 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = "ami-0b5eea76982371e91"
+  ami = "ami-00beae93a2d981137"
   instance_type = "t3.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
